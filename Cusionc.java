9180a5936a5642cd7d7aa854928a91864fa62373
madhavi <madhulakshmi996@gmail.com>
2022-10-19 21:31:58 +0530

    code

---
+++ Cusionc.java
@@ -0,0 +1,15 @@
+class Cusionc extends Woodenc
+{
+int colour=44; 
+@Override
+public  void seating()
+{
+System.out.println("sponge");
+}
+public static void main(String[]args)
+{
+Cusionc chair=new Cusionc();
+System.out.println(chair.colour);
+chair.seating();
+}
+}
